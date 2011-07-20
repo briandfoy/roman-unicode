1146d026d627ebb51af5975f0798a4ec1eb14df7
brian d foy <brian.d.foy@gmail.com>
2011-07-20 01:19:49 -0500

    Bump version

--- lib/Roman/Unicode.pm
+++ lib/Roman/Unicode.pm
@@ -12,7 +12,7 @@ use vars qw( $VERSION @EXPORT_OK );
 use Exporter 'import';
 @EXPORT_OK = qw( is_roman to_perl to_roman to_ascii );
 
-$VERSION = '1.02';
+$VERSION = '1.02_01';
 
 use Unicode::Normalize qw(NFKD);
 
